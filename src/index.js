7e65091d1806e3a0693cd5ced0c0a3b5606fadab
Henry Macfarlane <hjemacfarlane@gmail.com>
2020-06-08 14:29:23 +0100

    increments completed

--- src/index.js
+++ src/index.js
@@ -1,17 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { createStore } from "redux";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const initialState = {
+  player1: 0,
+  player2: 0,
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "INCREMENTPLAYER1": return { ...state, player1: state.player1 + 1 };
+    case "INCREMENTPLAYER2": return { ...state, player2: state.player2 + 1 };
+    default: return state;
+  }
+}
+const store = createStore(reducer, initialState);
+
+store.subscribe(() => {
+  // get the current state using the getState method 
+  // we can get the state, but not update it
+  let state = store.getState();
+
+  // for now, just log the new count
+  console.log(state.count)
+});
+
+const render = () => {
+  let state = store.getState();
+
+  ReactDOM.render(
+    <React.StrictMode>
+      <App
+        player1={state.player1}
+        player2={state.player2}
+        player1Increment={() => store.dispatch({ type: "INCREMENTPLAYER1" })}
+        player2Increment={() => store.dispatch({ type: "INCREMENTPLAYER2" })}
+      />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
+
+store.subscribe(render);
+render();
+
